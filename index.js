5330c2668a220f651273fa3619d5ea40b7b2f327
Micci Lorenzo <lorenzo@Air-di-Micci.homenet.telecomitalia.it>
2019-10-08 19:06:12 +0200

    upload by id e md5 encript

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 //Require Moduli
 const express = require("express");
 var app = express();
+const mongoose = require("mongoose");
+var md5 = require('md5');
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+
+const settings = require('./config/app-settings.js');
 const uploader = require("./lib/uploader");
 const func = require("./lib/update");
-const mongoose = require("mongoose");
-const settings = require('./config/app-settings.js');
 const upload = require("./lib/uploader").upload;
 Observer = require("./lib/Observer").Observer; // Require dell oggetto observer
 Subject  = require("./lib/Subject").Subject; // Require dell oggetto Subject
 
-//Object.create(Observer);
-//Object.create(Subject);
+Object.create(Observer);
+Object.create(Subject);
 
 State = require("./lib/State").State;
 List = require("./lib/List").List;
 
 // Instanzio le classi
-//lista = Object.create(List);
-//stato = Object.create(State);
+lista = Object.create(List);
+stato = Object.create(State);
 
-//List.update();
+
+const dom = new JSDOM(``, {
+  url: "localhost:3000",
+  referrer: "https://example.com/",
+  contentType: "text/html",
+  includeNodeLocations: true,
+  storageQuota: 10000000
+});
+
 
 
 mongoose.connect(settings.mongoConnString, {useNewUrlParser: true});
@@ -32,13 +46,44 @@ app.get('/', function (req, res) {
 
 app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
   const file = req.file
+  var nameEncripted = "";
   if (!file) {
     const error = new Error('Please upload a file')
     error.httpStatusCode = 400
     return next(error)
   }
     res.send(file)
+    nameEncripted = md5(file);
     console.log('file uploaded');
+
+})
+
+
+/*
+API di caricamento
+ - Permette di caricare un nuovo file.
+ - Permette di caricare una nuova versione di un file esistente di cui specifico un ID
+ - Se setto un flag a true, permette di caricare una nuova versione di un file esistente,
+   la cui esistenza viene controllata tramite MD5
+   (l'MD5 delle versioni caricate viene salvato a livello di record DB.
+   FA SEMPRE FEDE L'ULTIMA VERSIONE CARICATA).*/
+
+app.post('/uploadfileid', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.send(file)
+    // Non riesco a connettermi al db
+    if(dom.window.document.querySelector('.checkbox').checked) {
+    var encript = md5(file.originalname);
+    console.log('file uploaded\n' + file.originalname + "\n" + encript);
+  }
+  else {
+    onsole.log('file uploaded\n');
+  }
 
 })
 
